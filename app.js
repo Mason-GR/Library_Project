5a2fbee0385c9ed802b03c91201cf27cfa18514a
Mason-GR <mase210@gmail.com>
2022-02-07 10:08:20 -0500

    started project

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+class Book {
+  constructor(id, title, author, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+class Library {
+  constructor() {
+    this.bookcount = 2;
+    this.books = [
+      {
+        id: 1,
+        title: "Name of the Wind",
+        author: "Patrick Rothfuss",
+        read: true,
+      },
+    ];
+
+    this.markRead = this.markRead.bind(this);
+    this.adBook = this.addBook.bind(this);
+  }
+}
